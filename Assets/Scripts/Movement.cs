adedd819eaa327fc72ab05c5b834e8012e194f58
DESKTOP-DSRD6MR\Bas <gamingbas@hotmail.com>
2018-04-20 17:28:37 +0200

    basic movement.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    public float ForwardF = 1000F;
+    public float DownF = 10F;
+    public float DirectionalF = 1000F;
+  
+
+    void FixedUpdate()
+    {
+
+        rb.AddForce(0, 0, ForwardF * Time.deltaTime);
+
+        rb.AddForce(0, -DownF * Time.deltaTime, 0);
+
+        if (Input.GetKey("a"))
+        {
+
+            rb.AddForce(-DirectionalF, 0, 0);
+
+        }
+        if (Input.GetKey("d"))
+        {
+            
+            rb.AddForce(DirectionalF, 0, 0);
+
+        }
+    }
+}
